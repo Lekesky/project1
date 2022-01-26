6cad56fb15b6989dadf6b2d6bc9fd8e78d320c81
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-01-26 14:26:03 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("""
+                    (1) Add a task.
+                    (2) Remove a task.
+                    (3) Update a task.
+                    (4) List all tasks.
+                    (0) Exit.
+                    """);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Select an Action ");
+        int action = input.nextInt();
+
+        if()
+    }
+
+
+}
