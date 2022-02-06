7a415330514604a72228689c7c762e98e00cd062
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-02-06 13:52:49 -0500

    Created Methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,9 @@ import java.util.Scanner;
 public class Main {
     static Scanner input = new Scanner(System.in);
     static int action;  // Initialize action variable 
-    public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();     // Create the ArrayList
-        
+    static ArrayList<String> list = new ArrayList<>();     // Create the ArrayList
 
+    public static void prompt(){        //Prompt
         System.out.println("""
                         Please choose an option:
 
@@ -21,57 +20,66 @@ public class Main {
                         (4) List all tasks.
                         (0) Exit.
                         """);
-
-            
         System.out.println("Select an Action ");
+    }
+
+    public static void addTask(){       //Adds task
+        System.out.println("What task would you like to add?");
+        input.nextLine();
+        String itemAdd = input.nextLine();
+        list.add(itemAdd);
+        System.out.println("List has been updated!");
+    }
+
+    public static void removeTask(){        //Removes task
+        System.out.println("List: " + list);
+        System.out.println("What task would you like to remove?");
+        int itemRemove = input.nextInt();
+        list.remove(itemRemove);
+        System.out.println("List has been updated!");
+    }
+
+    public static void updateTask(){        //Updates task
+        System.out.println("List: " + list);
+        System.out.println("Select an element to update");
+        int itemUpdateNum = input.nextInt();
+        System.out.println("What task would you like to replace it with?");
+        input.nextLine();
+        String itemUpdate = input.nextLine();
+        list.set(itemUpdateNum, itemUpdate);
+        System.out.println("List has been updated!");
+    }
+
+    public static void listTask(){      //Prints all tasks in the ArrayList
+        System.out.println("List: " + list);
+    }
+
+    public static void main(String[] args) {
+
+        prompt();       //Initially prompt user with options before entering the while loop
         int action1 = input.nextInt();
-        action = action1;                           //Initially prompt user with options before entering the while loop
+        action = action1;                           
 
         while(action != 0 ){
-            if(action == 1){        //Adds task
-                System.out.println("What task would you like to add?");
-                input.nextLine();
-                String itemAdd = input.nextLine();
-                list.add(itemAdd);
-                System.out.println("List has been updated!");
+            if(action == 1){        
+                addTask();
 
-            }else if(action == 2){      //Removes task
-                System.out.println("List: " + list);
-                System.out.println("What task would you like to remove?");
-                int itemRemove = input.nextInt();
-                list.remove(itemRemove);
-                System.out.println("List has been updated!");
+            }else if(action == 2){
+                removeTask();
 
-            } else if(action == 3) {        //Updates task
-                System.out.println("List: " + list);
-                System.out.println("Select an element to update");
-                int itemUpdateNum = input.nextInt();
-                System.out.println("What task would you like to replace it with?");
-                input.nextLine();
-                String itemUpdate = input.nextLine();
-                list.set(itemUpdateNum, itemUpdate);
-                System.out.println("List has been updated!");
+            } else if(action == 3) {
+                updateTask();
                 
-            }else if(action == 4){      //Prints all tasks in the ArrayList
-                System.out.println("List: " + list);
+            }else if(action == 4){
+                listTask();
 
             }else if(action == 0){
                 System.out.println("Good-bye!");
                 break;
             }
-            System.out.println("""
-                        Please choose an option:
 
-                        (1) Add a task.
-                        (2) Remove a task.
-                        (3) Update a task.
-                        (4) List all tasks.
-                        (0) Exit.
-                        """);
-        
-            
-            System.out.println("Select an Action ");
-            int action2 = input.nextInt();      //Prompts user again after completing option
+            prompt();       //Prompts user again after completing option
+            int action2 = input.nextInt();      
             action = action2;       //Updates the action with new action
             
         }
